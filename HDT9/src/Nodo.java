5539fd09dce598464a941f5b1e44e0265c791ebd
Jose Hurtarte <JJHH121106@gmail.com>
2020-04-25 23:15:24 -0600

    Modificacion Nodo

--- HDT9/src/Nodo.java
+++ HDT9/src/Nodo.java
@@ -1,4 +1,10 @@
 
-public class Nodo {
+public class Nodo<K extends Comparable<K>, V>{
+        
+        public Nodo<K, V>  left, right;   // left and right subtrees
+        public ComparableAssociation<K, V> data;
 
-}
+        public Nodo(K key, V value) {
+            data = new ComparableAssociation<K,V>(key,value);
+        }
+    }
